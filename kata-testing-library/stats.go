00741876ec03acc5707860a0f8e32f782743c035
Alex Iraola <alex.iraola@gmail.com>
2024-02-16 10:06:44 +0100

    refactor avg

--- kata-testing-library/stats.go
+++ kata-testing-library/stats.go
@@ -11,11 +11,5 @@ func sum(numbers []int) int {
 }
 
 func avg(numbers []int) float64 {
-	sum := 0
-
-	for _, number := range numbers {
-		sum += number
-	}
-
-	return float64(sum) / float64(len(numbers))
+	return float64(sum(numbers)) / float64(len(numbers))
 }
