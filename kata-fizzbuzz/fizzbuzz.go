b9b0e04c4cf504b0eb266e951ed11651ae3058ee
Alex Iraola <alex.iraola@gmail.com>
2024-02-16 10:32:50 +0100

    Fizzbuzz kata

--- kata-fizzbuzz/fizzbuzz.go
+++ kata-fizzbuzz/fizzbuzz.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func fizzbuzz(number int) string {
-	return "1"
+	isDivisibleBy := func(divisor int) bool {
+		return number%divisor == 0
+	}
+
+	if isDivisibleBy(15) {
+		return "fizzbuzz"
+	}
+	if isDivisibleBy(3) {
+		return "fizz"
+	}
+	if isDivisibleBy(5) {
+		return "buzz"
+	}
+	return fmt.Sprint(number)
 }
--- kata-fizzbuzz/fizzbuzz_test.go
+++ kata-fizzbuzz/fizzbuzz_test.go
@@ -9,3 +9,59 @@ func TestOneShouldReturnOne(t *testing.T) {
 		t.Fatalf(`result should be "1", got %q`, result)
 	}
 }
+
+func TestThreeShouldReturnFizz(t *testing.T) {
+	result := fizzbuzz(3)
+
+	if result != "fizz" {
+		t.Fatalf(`result should be "fizz", got %q`, result)
+	}
+}
+
+func TestFiveShouldReturnBuzz(t *testing.T) {
+	result := fizzbuzz(5)
+
+	if result != "buzz" {
+		t.Fatalf(`result should be "buzz", got %q`, result)
+	}
+}
+
+func TestFifteenShouldReturnFizzbuzz(t *testing.T) {
+	result := fizzbuzz(15)
+
+	if result != "fizzbuzz" {
+		t.Fatalf(`result should be "fizzbuzz", got %q`, result)
+	}
+}
+
+func TestDivisibleByThreeShouldReturnFizz(t *testing.T) {
+	result := fizzbuzz(9)
+
+	if result != "fizz" {
+		t.Fatalf(`result should be "fizz", got %q`, result)
+	}
+}
+
+func TestDivisibleByFiveShouldReturnBuzz(t *testing.T) {
+	result := fizzbuzz(10)
+
+	if result != "buzz" {
+		t.Fatalf(`result should be "buzz", got %q`, result)
+	}
+}
+
+func TestDivisibleByThreeAndFiveShouldReturnFizzbuzz(t *testing.T) {
+	result := fizzbuzz(30)
+
+	if result != "fizzbuzz" {
+		t.Fatalf(`result should be "fizzbuzz", got %q`, result)
+	}
+}
+
+func TestNotDivisibleByThreeOrFiveShouldReturnTheSameNumber(t *testing.T) {
+	result := fizzbuzz(23)
+
+	if result != "23" {
+		t.Fatalf(`result should be "23", got %q`, result)
+	}
+}
