ab6eb0120b81d6a27e698879a86d331e0eea056f
Alex Iraola <alex.iraola@gmail.com>
2024-04-16 00:28:12 +0200

    String calculator resolved

--- kata-string-calculator/stringcalculator.go
+++ kata-string-calculator/stringcalculator.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 func stringCalculator(text string) int {
-	return 0
+	if text == "" {
+		return 0
+	}
+	separator, textWithoutPrefix := findSeparator(text)
+	numbers := strings.Split(textWithoutPrefix, separator)
+	sum := 0
+
+	for _, n := range numbers {
+		number, error := strconv.Atoi(n)
+
+		if error == nil {
+			sum += number
+		}
+	}
+
+	return sum
+}
+
+func findSeparator(text string) (string, string) {
+	if strings.HasPrefix(text, "//") {
+		separator := text[2:3]
+		numbers := text[4:]
+
+		return separator, numbers
+	}
+	return ",", text
 }
--- kata-string-calculator/stringcalculator_test.go
+++ kata-string-calculator/stringcalculator_test.go
@@ -9,3 +9,43 @@ func TestEmptyStringShouldReturnZero(t *testing.T) {
 		t.Fatalf(`expected %d, got %d`, 0, result)
 	}
 }
+
+func TestNumericStringShouldReturnNumber(t *testing.T) {
+	result := stringCalculator("3")
+
+	if result != 3 {
+		t.Fatalf(`expected %d, got %d`, 3, result)
+	}
+}
+
+func TestNumbersSeperatedByCommaShouldReturnSum(t *testing.T) {
+	result := stringCalculator("3,2,1")
+
+	if result != 6 {
+		t.Fatalf(`expected %d, got %d`, 6, result)
+	}
+}
+
+func TestNaNValuesShouldNotCount(t *testing.T) {
+	result := stringCalculator("3,a,2a,1")
+
+	if result != 4 {
+		t.Fatalf(`expected %d, got %d`, 4, result)
+	}
+}
+
+func TestCustomSeparator(t *testing.T) {
+	result := stringCalculator("//#/3#2")
+
+	if result != 5 {
+		t.Fatalf(`expected %d, got %d`, 5, result)
+	}
+}
+
+func TestCustomSeparatorInvalidNumbers(t *testing.T) {
+	result := stringCalculator("//#/3,2")
+
+	if result != 0 {
+		t.Fatalf(`expected %d, got %d`, 0, result)
+	}
+}
