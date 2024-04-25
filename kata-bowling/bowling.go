b32c3a2fb5b1c772a957fa02e8428ce398b0dd45
Alex Iraola <alex.iraola@gmail.com>
2024-04-26 00:16:52 +0200

    Kata bowling resolved

--- kata-bowling/bowling.go
+++ kata-bowling/bowling.go
@@ -1,5 +1,51 @@
 package main
 
+const MaxScore = 10
+
+type ScoreCalculator interface {
+	shift() int
+	calculate(game BowlingGame, index int) int
+}
+
+type StrikeCalculator struct{}
+
+func (c StrikeCalculator) shift() int {
+	return 1
+}
+
+func (c StrikeCalculator) calculate(game BowlingGame, index int) int {
+	return MaxScore + game.rolls[index+1] + game.rolls[index+2]
+}
+
+type SpareCalculator struct{}
+
+func (c SpareCalculator) shift() int {
+	return 2
+}
+
+func (c SpareCalculator) calculate(game BowlingGame, index int) int {
+	return MaxScore + game.rolls[index+2]
+}
+
+type DefaultCalculator struct{}
+
+func (c DefaultCalculator) shift() int {
+	return 2
+}
+
+func (c DefaultCalculator) calculate(game BowlingGame, index int) int {
+	return game.rolls[index] + game.rolls[index+1]
+}
+
+type Score struct {
+	score     int
+	rollIndex int
+}
+
+func (score Score) add(points int, addIndex int) Score {
+	return Score{score.score + points, score.rollIndex + addIndex}
+}
+
 type BowlingGame struct {
 	rolls []int
 }
@@ -9,11 +55,29 @@ func (game *BowlingGame) Roll(hits int) {
 }
 
 func (game BowlingGame) CalculateScore() int {
-	score := 0
+	score := Score{0, 0}
 
-	for _, roll := range game.rolls {
-		score += roll
+	for i := 0; i < 10; i++ {
+		calculator := game.getCalculator(score.rollIndex)
+		score = score.add(calculator.calculate(game, score.rollIndex), calculator.shift())
 	}
 
-	return score
+	return score.score
+}
+
+func (game BowlingGame) getCalculator(index int) ScoreCalculator {
+	if game.isStrike(index) {
+		return StrikeCalculator{}
+	} else if game.isSpare(index) {
+		return SpareCalculator{}
+	}
+	return DefaultCalculator{}
+}
+
+func (game BowlingGame) isStrike(index int) bool {
+	return game.rolls[index] == MaxScore
+}
+
+func (game BowlingGame) isSpare(index int) bool {
+	return game.rolls[index]+game.rolls[index+1] == MaxScore
 }
--- kata-bowling/bowling_test.go
+++ kata-bowling/bowling_test.go
@@ -10,15 +10,10 @@ func rollMany(game *BowlingGame, times int, pins int) {
 	}
 }
 
-func TestShoulBeAbleToRollABall(t *testing.T) {
-	game := BowlingGame{}
-
-	game.Roll(0)
-
-	result := game.rolls[0]
-
-	if result != 0 {
-		t.Fatalf(`expected %d, got %d`, 0, result)
+func rollManyCouple(game *BowlingGame, times int, first int, second int) {
+	for i := 0; i < times; i++ {
+		game.Roll(first)
+		game.Roll(second)
 	}
 }
 
@@ -43,5 +38,72 @@ func TestOneHitEveryShotShouldGet20Points(t *testing.T) {
 
 	if result != 20 {
 		t.Fatalf(`expected %d, got %d`, 20, result)
+	}
+}
+
+func TestCalculatesTheScoreForAGivenSpareExtraBall(t *testing.T) {
+	game := BowlingGame{}
+
+	game.Roll(5)
+	game.Roll(5)
+	game.Roll(5)
+	rollMany(&game, 17, 0)
+
+	result := game.CalculateScore()
+
+	if result != 20 {
+		t.Fatalf(`expected %d, got %d`, 20, result)
+	}
+}
+
+func TestCalculatesTheScoreForAGivenStrikeExtraBall(t *testing.T) {
+	game := BowlingGame{}
+
+	game.Roll(10)
+	game.Roll(2)
+	game.Roll(3)
+	rollMany(&game, 16, 0)
+
+	result := game.CalculateScore()
+
+	if result != 20 {
+		t.Fatalf(`expected %d, got %d`, 20, result)
+	}
+}
+
+func TestCalculatesAPerfectGame(t *testing.T) {
+	game := BowlingGame{}
+
+	rollMany(&game, 12, 10)
+
+	result := game.CalculateScore()
+
+	if result != 300 {
+		t.Fatalf(`expected %d, got %d`, 300, result)
+	}
+}
+
+func TestCalculatesASpareInTheLastTurn(t *testing.T) {
+	game := BowlingGame{}
+
+	rollMany(&game, 21, 5)
+
+	result := game.CalculateScore()
+
+	if result != 150 {
+		t.Fatalf(`expected %d, got %d`, 150, result)
+	}
+}
+
+func TestCalculatesASpareWithDifferentScoreTheLastTurn(t *testing.T) {
+	game := BowlingGame{}
+
+	rollManyCouple(&game, 10, 8, 2)
+	game.Roll(8)
+
+	result := game.CalculateScore()
+
+	if result != 180 {
+		t.Fatalf(`expected %d, got %d`, 180, result)
 	}
 }
